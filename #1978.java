86a7574195bfb210c80a6820275db44aec16593e
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2022-12-29 03:35:03 +0900

    #1978.java

---
+++ #1978.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static int isPrime(int n) {
+		if(n==1)
+			return 0;
+		
+		for(int i=2; i<=n/2; i++) {
+			if(n%i==0)
+				return 0;
+		}
+		return 1;
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		
+		int count=0;
+		
+		for(int i=1; i<=a; i++) {
+			int num=sc.nextInt();
+			count+=isPrime(num);
+		}
+		
+		System.out.println(count);
+		
+	}
+}
