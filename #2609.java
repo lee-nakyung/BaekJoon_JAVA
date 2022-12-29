728fdc4d42e4eb3dad2e1728a0b180751cf549e3
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2022-12-30 04:14:53 +0900

    재귀함수 활용, 최소공배수, 최대공약수

--- #2609.java
+++ #2609.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int gcd(int a, int b) {
+	static int gcd(int a, int b) { //최소공배수
 		if(a==0)
 			return b;
 		return gcd(b%a,a);
 	}
 	
-	static int lcm(int a, int b) {
+	static int lcm(int a, int b) { //최대공약수
 		return (a/gcd(a,b)*b);
 		
 	}
